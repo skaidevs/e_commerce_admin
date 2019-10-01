19bd3ed237f0edaf192a329e05ebc0c83d9768f7
skai <skaidevs@gmail.com>
2019-09-30 23:59:57 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/skaidevs/e_commerce_admin/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.skaidevs.e_commerce_admin
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:e_commerce_admin/screens/admin.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Admin(),
+    ));
